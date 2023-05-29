eb7690c40a4119334aa0d05f9456733f5c89a53f
phuhung9292 <phuhungx1810@gmail.com>
2023-05-29 14:00:08 +0700

    Create Model

---
+++ ITManagementSystemBackend/BusinessObject/BaseSalary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class BaseSalary
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public Decimal _baseSalary { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        [Required]
+        public string Status { get; set; }
+        [Required]
+        public int EmployeeId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
