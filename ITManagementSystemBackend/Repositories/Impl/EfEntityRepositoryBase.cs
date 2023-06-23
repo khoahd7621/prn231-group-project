23a19bf36fa75195533bcc2969de38c96e0d2965
HungPD <hungpd170501@gmail.com>
2023-06-24 02:28:09 +0700

    ef Core Generic Repository

---
+++ ITManagementSystemBackend/Repositories/Impl/EfEntityRepositoryBase.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject;
+
+namespace Repositories.Impl
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, new()
+          where TContext : DbContext
+    {
+        protected readonly DbContext _context = new MyDbContext();
+        public EfEntityRepositoryBase()
+        {
+        }
+        public TEntity Add(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public IEnumerable<TEntity> AddRange(IEnumerable<TEntity> entities)
+        {
+            _context.Set<TEntity>().AddRange(entities);
+            _context.SaveChanges();
+            return entities;
+        }
+        public async Task<IEnumerable<TEntity>> AddRangeAsync(IEnumerable<TEntity> entities)
+        {
+            _context.Set<TEntity>().AddRange(entities);
+            await _context.SaveChangesAsync();
+            return entities;
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public async Task RemoveAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            return _context.Set<TEntity>().FirstOrDefault(filter);
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await _context.Set<TEntity>().FirstOrDefaultAsync(filter);
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Entry<TEntity>(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+        public async Task UpdateAsync(TEntity entity)
+        {
+            _context.Entry<TEntity>(entity).State = EntityState.Modified;
+            //_context.Set<TEntity>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateRangeAsync(IEnumerable<TEntity> entities)
+        {
+            _context.Set<TEntity>().UpdateRange(entities);
+            await _context.SaveChangesAsync();
+        }
+
+        public int Count()
+        {
+            return _context.Set<TEntity>().Count();
+        }
+
+        public async Task<int> CountAsync()
+        {
+            return await _context.Set<TEntity>().CountAsync();
+        }
+
+        public void UpdateRange(IEnumerable<TEntity> entities)
+        {
+            _context.Set<TEntity>().UpdateRange(entities);
+            _context.SaveChanges();
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            _context.Set<TEntity>().RemoveRange(entities);
+            _context.SaveChanges();
+        }
+
+        public async Task RemoveRangeAsync(IEnumerable<TEntity> entities)
+        {
+            _context.Set<TEntity>().RemoveRange(entities);
+            await _context.SaveChangesAsync();
+        }
+
+        public ICollection<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, ICollection<TEntity>> options = null, string includeProperties = null)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            if (options != null)
+            {
+                return options(query).ToList();
+            }
+
+            return query.ToList();
+        }
+
+        public async Task<ICollection<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, ICollection<TEntity>> options = null, string includeProperties = null)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            if (options != null)
+            {
+                return options(query).ToList();
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public TEntity GetFirstOrDefault(Expression<Func<TEntity, bool>> filter = null, string includeProperties = null)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+            if (filter != null)
+            {
+                query = query.AsNoTracking().Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            return query.AsNoTracking().FirstOrDefault();
+        }
+        public Task<TEntity> GetFirstOrDefaultAsync(Expression<Func<TEntity, bool>> filter = null, string includeProperties = null)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+            if (filter != null)
+            {
+                query = query.AsNoTracking().Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            return query.AsNoTracking().FirstOrDefaultAsync();
+        }
+    }
+}
