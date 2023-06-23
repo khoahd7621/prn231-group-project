b2f62c16cd363db0dfa58593b2f9aef5d911e205
HungPD <hungpd170501@gmail.com>
2023-06-24 02:29:40 +0700

    add TakeLeaveRepo

---
+++ ITManagementSystemBackend/Repositories/ITakeLeaveRepository.cs
@@ -0,0 +1,21 @@
+﻿using BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public interface ITakeLeaveRepository
+    {
+        void SaveTakeLeave(TakeLeave takeLeave);
+        TakeLeave GetTakeLeaveById(int id);
+        IEnumerable<TakeLeave> GetAllTakeLeavesByEmployeeIdEqual(int id);
+        IEnumerable<TakeLeave> GetAllTakeLeavesByDateBetween(DateTime startDate, DateTime endDate);
+        TakeLeave GetTakeLeaveByDateEqualAndEmployeeIdEqual (DateTime date, int employeeId);
+        IEnumerable<TakeLeave> GetTakeLeaves();
+        void UpdateTakeLeave(TakeLeave takeLeave);
+        void DeleteTakeLeave(TakeLeave takeLeave);
+    }
+}
---
+++ ITManagementSystemBackend/Repositories/Impl/TakeLeaveRepository.cs
@@ -0,0 +1,32 @@
+﻿using BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Impl
+{
+    public class TakeLeaveRepository : EfEntityRepositoryBase<TakeLeave, MyDbContext>, ITakeLeaveRepository
+    {
+        public void DeleteTakeLeave(TakeLeave takeLeave) => Remove(takeLeave);
+
+
+        public IEnumerable<TakeLeave> GetAllTakeLeavesByDateBetween(DateTime startDate, DateTime endDate) => (IEnumerable<TakeLeave>)GetAll(filter: tl => tl.Date >= startDate && tl.Date <= endDate);
+
+
+        public IEnumerable<TakeLeave> GetAllTakeLeavesByEmployeeIdEqual(int id) => (IEnumerable<TakeLeave>)GetAll(filter: tl => tl.EmployeeId == id);
+
+
+        public TakeLeave GetTakeLeaveByDateEqualAndEmployeeIdEqual(DateTime date, int employeeId) => GetFirstOrDefault(filter: tl => tl.Date.Equals(date) && tl.EmployeeId == employeeId && tl.Status.Equals(TakeLeaveStatus.APPROVED), includeProperties: "User");
+
+        public TakeLeave GetTakeLeaveById(int id) => GetFirstOrDefault(filter: tl => tl.Id == id, includeProperties: "User");
+
+
+        public IEnumerable<TakeLeave> GetTakeLeaves() => (IEnumerable<TakeLeave>)GetAll(includeProperties: "User");
+
+        public void SaveTakeLeave(TakeLeave takeLeave) => Add(takeLeave);
+
+        public void UpdateTakeLeave(TakeLeave takeLeave) => Update(takeLeave);
+    }
+}
