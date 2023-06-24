4f4584725b4ce5c5a1681f580d348f9fdb775ad1
Kiên Phạm <101553209+kienpham2k1@users.noreply.github.com>
2023-06-24 08:02:47 +0700

    Feature/crud attendence (#7)
    * Implement manage position layout
    
    * Implement manage level layout
    
    * Clean project
    
    * crud for attendance
    
    * update DAO, add range for hour and otHour
    
    * update create attendance only in 1 day
    
    ---------
    
    Co-authored-by: khoahd7621 <hoangdangkhoa7621@gmail.com>

---
+++ ITManagementSystemBackend/ITManagementSystemWebAPI/Controllers/AttendanceController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Repositories.Impl;
+using Repositories;
+using BusinessObject;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using DataTransfer.Request;
+using BusinessObject.Enum;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ITManagementSystemWebAPI.Controllers
+{
+    public class AttendanceController : ODataController
+    {
+        private readonly IAttendanceRepository attendanceRepository = new AttendaceReposiory();
+        private readonly IEmployeeRepository employeeRepository = new EmployeeRepository();
+
+        [EnableQuery]
+        public IActionResult Get() => Ok(attendanceRepository.GetAttendences());
+        public ActionResult<Attendance> Get(int key)
+        {
+            var item = attendanceRepository.FindAttendanceById(key);
+
+            if (item == null) return NotFound();
+
+            return Ok(item);
+        }
+
+        public ActionResult Post([FromBody] AttendanceReq attendanceRq)
+        {
+            var tempAttendace = attendanceRepository.FindAttendanceByUserAndDay(attendanceRq.EmployeeId, attendanceRq.Date);
+
+            if (tempAttendace != null)
+            {
+                return BadRequest("Attendace already exists.");
+            }
+            Attendance newAttendance = new Attendance
+            {
+                Date = attendanceRq.Date,
+                Hour = attendanceRq.Hour,
+                OTHour = attendanceRq.OTHour,
+                Type = attendanceRq.Type,
+                EmployeeId = attendanceRq.EmployeeId
+            };
+            attendanceRepository.SaveAttendance(newAttendance);
+
+            return Created(newAttendance);
+        }
+
+        public IActionResult Put(int key, [FromBody] AttendanceReq attendanceReq)
+        {
+            var attendance = attendanceRepository.FindAttendanceById(key);
+
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+            if (attendance.Status != EnumList.AttendanceStatus.Deleted)
+            {
+                attendance.Date = attendanceReq.Date;
+                attendance.Hour = attendanceReq.Hour;
+                attendance.OTHour = attendanceReq.OTHour;
+                attendance.Type = attendanceReq.Type;
+                attendance.EmployeeId = attendanceReq.EmployeeId;
+                //}
+                //else return BadRequest();
+            }
+            else
+            {
+                return BadRequest("Status it not delete");
+            }
+
+            attendanceRepository.UpdateAttendance(attendance);
+
+            return Updated(attendance);
+        }
+        public IActionResult Patch(int key, EnumList.AttendanceStatus attendanceStatus)
+        {
+            var attendance = attendanceRepository.FindAttendanceById(key);
+
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            if (attendance.Status == attendanceStatus)
+            {
+                return BadRequest("Change it staus");
+            }
+
+            attendanceRepository.UpdateStatusAttendance(key, attendanceStatus);
+            return Ok();
+        }
+    }
+}
---
+++ ITManagementSystemBackend/Repositories/IAttendanceRepository.cs
@@ -0,0 +1,24 @@
+﻿using BusinessObject;
+using BusinessObject.Enum;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public interface IAttendanceRepository
+    {
+        public List<Attendance> GetAttendences();
+        public Attendance FindAttendanceById(int attendanceId);
+        public List<Attendance> FindAttendanceByUserAndTime(int userId, DateTime timeBegin, DateTime timeEnd);
+        public Attendance FindAttendanceByUserAndDay(int userId, DateTime time);
+        public void UpdateStatusAttendance(int id, EnumList.AttendanceStatus attendanceStatus);
+        public Attendance FindAttendanceByUser(int userId);
+        public void SaveAttendance(Attendance Attendance);
+        public void UpdateAttendance(Attendance Attendance);
+        public void DeleteAttendance(Attendance Attendance);
+    }
+}
