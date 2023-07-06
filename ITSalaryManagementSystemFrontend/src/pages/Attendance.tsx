b1049d85e7d212eb6caf312b4970129245902a36
Kiên Phạm <101553209+kienpham2k1@users.noreply.github.com>
2023-07-06 12:23:32 +0700

    Feature/attendance search (#42)
    * filter status & type
    
    * filter

--- ITSalaryManagementSystemFrontend/src/pages/Attendance.tsx
+++ ITSalaryManagementSystemFrontend/src/pages/Attendance.tsx
@@ -1,26 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import dayjs from "dayjs";
 import React, { useEffect } from "react";
 
-import { Button, Input, Space, Table, Tag } from "antd";
+import { Button, DatePicker, Input, Space, Table, Tag } from "antd";
 import type { ColumnsType } from "antd/es/table";
 
 import { AttendanceStatus, AttendanceType } from "../constants/enum";
 import AttendanceApis from "../modules/attendance/apis/AttendanceApis";
-import {
-  AttendanceStatusTag,
-  CreateModal,
-  DeleteModal,
-  EditModal,
-} from "../modules/attendance/components";
+import { AttendanceStatusTag, CreateModal, DeleteModal, EditModal } from "../modules/attendance/components";
 import { AttendanceModel } from "../modules/attendance/models";
+import { SearchOutlined } from "@ant-design/icons";
 
 type DataType = {
   key: number;
 } & AttendanceModel;
 
-const { Search } = Input;
-
 export const Attendance: React.FC = () => {
+  const pageSizeOptions = [5, 10, 20, 50];
+
   const columns: ColumnsType<DataType> = [
     {
       title: "Employee Name",
@@ -28,6 +25,38 @@ export const Attendance: React.FC = () => {
       dataIndex: ["User", "EmployeeName"],
       sortDirections: ["descend", "ascend"],
       sorter: (a, b) => a.EmployeeId - b.EmployeeId,
+      filterMultiple: false,
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+          <Input
+            placeholder="Search name"
+            value={selectedKeys[0]}
+            onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+            onPressEnter={() => confirm}
+            style={{ marginBottom: 8, display: "block" }}
+          />
+          <Space>
+            <Button
+              type="primary"
+              onClick={() => {
+                confirm();
+              }}
+              icon={<SearchOutlined />}
+              size="small"
+              style={{ width: 90 }}>
+              Search
+            </Button>
+            <Button onClick={clearFilters} size="small" style={{ width: 90 }}>
+              Reset
+            </Button>
+          </Space>
+        </div>
+      ),
+      filterIcon: (filtered: boolean) => <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />,
+      onFilter: (value, record) =>
+        (record.User as any).EmployeeName.toString()
+          .toLowerCase()
+          .includes((value as string).toLowerCase()),
       render: (value: string, record: DataType) => (
         <div>
           {(record.User as any).EmployeeCode} - {value}
@@ -37,6 +66,51 @@ export const Attendance: React.FC = () => {
     {
       title: "Date",
       dataIndex: "Date",
+      filterMultiple: false,
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        <div style={{ padding: 10 }} onKeyDown={(e) => e.stopPropagation()}>
+          <RangePicker
+            value={selectedKeys[0]}
+            onChange={(e) => {
+              setSelectedKeys(e ? [e] : []);
+            }}
+            onPressEnter={() => confirm}
+            style={{ marginBottom: 10, display: "flex" }}
+          />
+
+          <Space style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Button
+              type="primary"
+              onClick={() => {
+                confirm();
+              }}
+              icon={<SearchOutlined />}
+              size="small"
+              style={{ width: 90 }}>
+              Search
+            </Button>
+            <Button
+              onClick={() => {
+                clearFilters();
+                confirm();
+              }}
+              size="small"
+              style={{ width: 90 }}>
+              Reset
+            </Button>
+          </Space>
+        </div>
+      ),
+      filterIcon: (filtered: boolean) => <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />,
+      onFilter: (value, record) => {
+        const recordDate = dayjs(record.Date);
+        const startDate = value[0];
+        const endDate = value[1];
+        return startDate && endDate
+          ? (recordDate.isAfter(startDate, "day") || recordDate.isSame(startDate, "day")) &&
+              (recordDate.isBefore(endDate, "day") || recordDate.isSame(startDate, "day"))
+          : true;
+      },
       sortDirections: ["descend", "ascend"],
       sorter: (a, b) => {
         const dateA = new Date(a.Date.toString());
@@ -64,6 +138,22 @@ export const Attendance: React.FC = () => {
       dataIndex: "Status",
       sortDirections: ["descend", "ascend"],
       sorter: (a, b) => a.Status - b.Status,
+      filters: [
+        {
+          text: AttendanceStatus[0],
+          value: AttendanceStatus[0],
+        },
+        {
+          text: AttendanceStatus[1],
+          value: AttendanceStatus[1],
+        },
+        {
+          text: AttendanceStatus[2],
+          value: AttendanceStatus[2],
+        },
+      ],
+      filterMode: "tree",
+      onFilter: (value: any, record) => record.Status.toString() === value.toString(),
       render: (value: AttendanceStatus) => (
         <div>
           <AttendanceStatusTag status={value} />
@@ -73,8 +163,20 @@ export const Attendance: React.FC = () => {
     {
       title: "Type",
       dataIndex: "Type",
-      sortDirections: ["descend", "ascend"],
+      filters: [
+        {
+          text: AttendanceType[0],
+          value: AttendanceType.Offline.toString(),
+        },
+        {
+          text: AttendanceType[1],
+          value: AttendanceType.Online.toString(),
+        },
+      ],
+      filterMode: "tree",
+      onFilter: (value: any, record) => record.Type == parseInt(value),
       sorter: (a, b) => a.Type - b.Type,
+      sortDirections: ["descend", "ascend"],
       render: (value: AttendanceType) => {
         switch (value) {
           case AttendanceType.Online.valueOf():
@@ -93,15 +195,14 @@ export const Attendance: React.FC = () => {
       },
     },
     {
-      width: "20px",
+      key: "action",
+      width: "350px",
       render: (_, record) => {
         switch (record.Status.valueOf()) {
           case (AttendanceStatus[0] as any).valueOf():
             return (
               <Space>
-                <Button
-                  style={{ backgroundColor: "#00cc00", color: "white" }}
-                  onClick={() => handleApprove(record.Id)}>
+                <Button style={{ backgroundColor: "#00cc00", color: "white" }} onClick={() => handleApprove(record.Id)}>
                   Aprrove
                 </Button>
                 <Button danger onClick={() => handleReject(record.Id)}>
@@ -123,12 +224,6 @@ export const Attendance: React.FC = () => {
       },
     },
   ];
-
-  const [limit, setLimit] = React.useState<number>(5);
-  const [page, setPage] = React.useState<number>(1);
-  const [total, setTotal] = React.useState<number>(0);
-
-  const [current, setCurrent] = React.useState<DataType | null>(null);
   const [loading, setLoading] = React.useState<boolean>(true);
   const [attendances, setAttendances] = React.useState<DataType[]>([]);
 
@@ -141,14 +236,12 @@ export const Attendance: React.FC = () => {
     AttendanceApis.getAll()
       .then((res) => {
         setAttendances(res.value.map((item) => ({ ...item, key: item.Id })));
-        setTotal(res.value.length);
       })
       .catch((err) => console.log(err));
     setLoading(false);
   };
 
   const successCallback = () => {
-    setPage(1);
     fetchAttendances();
   };
 
@@ -167,36 +260,26 @@ export const Attendance: React.FC = () => {
       })
       .catch((err) => console.log(err));
   };
-
+  const { RangePicker } = DatePicker;
   return (
     <>
       <div
         style={{
           display: "flex",
-          justifyContent: "space-between",
+          justifyContent: "flex-end",
           marginBottom: 16,
         }}>
-        <Search
-          placeholder="Search attendance"
-          style={{
-            width: 400,
-          }}
-          allowClear
-        />
         <CreateModal isEmp={false} successCallback={successCallback} />
       </div>
       <Table
+        size="large"
         columns={columns}
         dataSource={attendances}
         loading={loading}
         pagination={{
-          current: page,
-          pageSize: limit,
-          total: total,
-          onChange: (page, pageSize) => {
-            setPage(page);
-            setLimit(pageSize || 5);
-          },
+          pageSizeOptions,
+          showSizeChanger: true,
+          position: ["topLeft"],
         }}
       />
     </>
--- ITSalaryManagementSystemFrontend/src/pages/EmpAttendance.tsx
+++ ITSalaryManagementSystemFrontend/src/pages/EmpAttendance.tsx
@@ -1,30 +1,68 @@
 import dayjs from "dayjs";
-import React, { useEffect } from "react";
-
-import { Input, Space, Table, Tag } from "antd";
-import type { ColumnsType } from "antd/es/table";
-
-import { AttendanceStatus, AttendanceType } from "../constants/enum";
 import AttendanceApis from "../modules/attendance/apis/AttendanceApis";
-import {
-  AttendanceStatusTag,
-  CreateModal,
-  DeleteModal,
-  EditModal,
-} from "../modules/attendance/components";
+
+import React, { useEffect } from "react";
+import type { ColumnsType } from "antd/es/table";
+import { Button, DatePicker, Space, Table, Tag } from "antd";
+import { AttendanceStatus, AttendanceType } from "../constants/enum";
+import { AttendanceStatusTag, CreateModal, DeleteModal, EditModal } from "../modules/attendance/components";
 import { AttendanceModel } from "../modules/attendance/models";
+import { SearchOutlined } from "@ant-design/icons";
 
 type DataType = {
   key: number;
 } & AttendanceModel;
-
-const { Search } = Input;
-
+const { RangePicker } = DatePicker;
 export const EmpAttendance: React.FC = () => {
   const columns: ColumnsType<DataType> = [
     {
       title: "Date",
       dataIndex: "Date",
+      filterMultiple: false,
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        <div style={{ padding: 10 }} onKeyDown={(e) => e.stopPropagation()}>
+          <RangePicker
+            value={selectedKeys[0]}
+            onChange={(e) => {
+              setSelectedKeys(e ? [e] : []);
+            }}
+            onPressEnter={() => confirm}
+            style={{ marginBottom: 10, display: "flex" }}
+          />
+
+          <Space style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Button
+              type="primary"
+              onClick={() => {
+                confirm();
+              }}
+              icon={<SearchOutlined />}
+              size="small"
+              style={{ width: 90 }}>
+              Search
+            </Button>
+            <Button
+              onClick={() => {
+                clearFilters();
+                confirm();
+              }}
+              size="small"
+              style={{ width: 90 }}>
+              Reset
+            </Button>
+          </Space>
+        </div>
+      ),
+      filterIcon: (filtered: boolean) => <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />,
+      onFilter: (value, record) => {
+        const recordDate = dayjs(record.Date);
+        const startDate = value[0];
+        const endDate = value[1];
+        return startDate && endDate
+          ? (recordDate.isAfter(startDate, "day") || recordDate.isSame(startDate, "day")) &&
+              (recordDate.isBefore(endDate, "day") || recordDate.isSame(startDate, "day"))
+          : true;
+      },
       sortDirections: ["descend", "ascend"],
       sorter: (a, b) => {
         const dateA = new Date(a.Date.toString());
@@ -51,6 +89,22 @@ export const EmpAttendance: React.FC = () => {
       title: "Status",
       dataIndex: "Status",
       sortDirections: ["descend", "ascend"],
+      filters: [
+        {
+          text: AttendanceStatus[0],
+          value: AttendanceStatus[0],
+        },
+        {
+          text: AttendanceStatus[1],
+          value: AttendanceStatus[1],
+        },
+        {
+          text: AttendanceStatus[2],
+          value: AttendanceStatus[2],
+        },
+      ],
+      filterMode: "tree",
+      onFilter: (value, record) => record.Status.toString() === value.toString(),
       sorter: (a, b) => a.Status - b.Status,
       render: (value: AttendanceStatus) => (
         <div>
@@ -61,6 +115,18 @@ export const EmpAttendance: React.FC = () => {
     {
       title: "Type",
       dataIndex: "Type",
+      filters: [
+        {
+          text: AttendanceType[0],
+          value: AttendanceType.Offline.toString(),
+        },
+        {
+          text: AttendanceType[1],
+          value: AttendanceType.Online.toString(),
+        },
+      ],
+      filterMode: "tree",
+      onFilter: (value, record) => record.Type == parseInt(value),
       sortDirections: ["descend", "ascend"],
       sorter: (a, b) => a.Type - b.Type,
       render: (value: AttendanceType) => {
@@ -81,9 +147,9 @@ export const EmpAttendance: React.FC = () => {
       },
     },
     {
-      width: "20px",
+      width: "200px",
       render: (_, record) => {
-        if (record.Status != (AttendanceStatus[1] as any).valueOf())
+        if (record.Status != AttendanceStatus[1].valueOf())
           return (
             <Space>
               <EditModal data={record} successCallback={fetchAttendances} />
@@ -94,12 +160,9 @@ export const EmpAttendance: React.FC = () => {
     },
   ];
 
-  const [limit, setLimit] = React.useState<number>(5);
-  const [page, setPage] = React.useState<number>(1);
-  const [total, setTotal] = React.useState<number>(0);
   const [loading, setLoading] = React.useState<boolean>(true);
   const [attendances, setAttendances] = React.useState<DataType[]>([]);
-
+  const pageSizeOptions = [5, 10, 20, 50];
   useEffect(() => {
     fetchAttendances();
   }, []);
@@ -108,17 +171,13 @@ export const EmpAttendance: React.FC = () => {
     setLoading(true);
     AttendanceApis.getEmployee("?$expand=Attendances($expand=User)")
       .then((res) => {
-        setAttendances(
-          (res as any).Attendances.map((item: { Id: any }) => ({ ...item, key: item.Id }))
-        );
-        setTotal((res as any).Attendances.length);
+        setAttendances(res.Attendances.map((item: { Id: any }) => ({ ...item, key: item.Id })));
       })
       .catch((err) => console.log(err));
     setLoading(false);
   };
 
   const successCallback = () => {
-    setPage(1);
     fetchAttendances();
   };
   return (
@@ -126,30 +185,20 @@ export const EmpAttendance: React.FC = () => {
       <div
         style={{
           display: "flex",
-          justifyContent: "space-between",
+          justifyContent: "flex-end",
           marginBottom: 16,
         }}>
-        <Search
-          placeholder="Search attendance"
-          style={{
-            width: 400,
-          }}
-          allowClear
-        />
         <CreateModal isEmp={true} successCallback={successCallback} />
       </div>
       <Table
+        size="large"
         columns={columns}
         dataSource={attendances}
         loading={loading}
         pagination={{
-          current: page,
-          pageSize: limit,
-          total: total,
-          onChange: (page, pageSize) => {
-            setPage(page);
-            setLimit(pageSize || 5);
-          },
+          pageSizeOptions,
+          showSizeChanger: true,
+          position: ["topLeft"],
         }}
       />
     </>
