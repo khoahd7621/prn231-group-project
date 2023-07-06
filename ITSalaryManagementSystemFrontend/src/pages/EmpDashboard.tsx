200336c66c5c57386af1a43720a935b6f732a5c9
ptKien <phamthanhkien2k1@gmail.com>
2023-07-06 09:58:28 +0700

    fix ui empl

--- ITSalaryManagementSystemFrontend/src/pages/EmpDashboard.tsx
+++ ITSalaryManagementSystemFrontend/src/pages/EmpDashboard.tsx
@@ -1,16 +1,17 @@
 import dayjs from "dayjs";
 import React, { useEffect } from "react";
 
-import { Card, Col, Descriptions, Modal, Progress, Row, Table, Tag, Typography } from "antd";
 import type { ColumnsType } from "antd/es/table";
+import AttendanceApis from "../modules/attendance/apis/AttendanceApis";
+
+import { Button, Card, Col, DatePicker, Descriptions, Modal, Progress, Row, Space, Table, Tag, Typography } from "antd";
 import { BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, Title, Tooltip } from "chart.js";
 import { Bar } from "react-chartjs-2";
-
-import { ContractStatus } from "../constants/enum";
-import AttendanceApis from "../modules/attendance/apis/AttendanceApis";
+import { AttendanceStatus, AttendanceType, ContractStatus } from "../constants/enum";
 import { AttendanceModel, AttendanceStisticModel } from "../modules/attendance/models";
 import { ContractModel } from "../modules/contract/models";
-
+import { AttendanceStatusTag } from "../modules/attendance/components";
+import { SearchOutlined } from "@ant-design/icons";
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 type DataAttendanceType = {
@@ -26,6 +27,7 @@ type DataContracType = {
 } & ContractModel;
 
 const { Title: AntdTitle, Text } = Typography;
+const { RangePicker } = DatePicker;
 
 const optionsChart = {
   responsive: true,
@@ -35,14 +37,15 @@ const optionsChart = {
     },
   },
 };
-
 const labels = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
+const pageSizeOptions = [5, 10, 20, 50];
 
 export const EmpDashBoard: React.FC = () => {
   const [loading, setLoading] = React.useState<boolean>(true);
 
   const [attendancesWeek, setAttendancesWeek] = React.useState<DataAttendanceType[]>([]);
-  const [attendancesMonth, setAttendancesMonth] = React.useState<DataAttendanceStatisticType[]>([]);
+  const [attendancesMonthly, setAttendancesMonthly] = React.useState<DataAttendanceType[]>([]);
+  const [attendancesWeekly, setAttendancesWeekly] = React.useState<DataAttendanceStatisticType[]>([]);
   const [contracts, setContracts] = React.useState<DataContracType[]>([]);
   const [contractSelect, setContractSelect] = React.useState<ContractModel>();
   const [isModalOpen, setIsModalOpen] = React.useState(false);
@@ -50,6 +53,7 @@ export const EmpDashBoard: React.FC = () => {
   const [unApproveHours, setUnApproveHours] = React.useState<number>(0);
   const [previousWeekHours, setPreviousWeekHours] = React.useState<number>(0);
   const [previousOrvertimeWeekHours, setPreviiousOrvertimeWeekHours] = React.useState<number>(0);
+  //#region 
   const chartData = {
     labels,
     datasets: [
@@ -67,7 +71,7 @@ export const EmpDashBoard: React.FC = () => {
           }
           return value;
         }),
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
+        backgroundColor: "blue",
       },
       {
         label: "OT Hour",
@@ -83,16 +87,23 @@ export const EmpDashBoard: React.FC = () => {
           }
           return value;
         }),
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
+        backgroundColor: "red",
       },
     ],
   };
 
-  const columns: ColumnsType<DataAttendanceStatisticType> = [
+  const columnsWeelyTable: ColumnsType<DataAttendanceStatisticType> = [
     {
       title: "Date",
-      dataIndex: "Date",
-      render: (value: string) => <div>{dayjs(value).format("YYYY-MM-DD ")}</div>,
+      dataIndex: "key",
+      defaultSortOrder: "ascend",
+      render: (value: string) => <div>{value}</div>,
+      sortDirections: ["descend", "ascend"],
+      sorter: (a, b) => {
+        const dateA = new Date(a.Date.toString());
+        const dateB = new Date(b.Date.toString());
+        return dateA.getTime() - dateB.getTime();
+      },
     },
     {
       title: "Expected",
@@ -103,19 +114,137 @@ export const EmpDashBoard: React.FC = () => {
       dataIndex: "Logged",
     },
     {
-      sortDirections: ["descend", "ascend"],
       render: (value: AttendanceStisticModel) => (
         <div>
-          <Progress
-            type="circle"
-            percent={(value.Logged / value.Expected) * 100}
-            size={30}
-          />
+          <Progress type="circle" percent={(value.Logged / value.Expected) * 100} size={30} />
         </div>
       ),
     },
+    {
+      render: (value: AttendanceStisticModel) =>
+        value.Logged > value.Expected ? (
+          <div>
+            <Progress
+              strokeColor={{ "0%": "red", "100%": "#red" }}
+              type="circle"
+              percent={((value.Logged - value.Expected) / value.Expected) * 100}
+              size={30}
+            />
+          </div>
+        ) : (
+          <></>
+        ),
+    },
   ];
 
+  const columnsMonthly: ColumnsType<DataAttendanceType> = [
+    {
+      title: "Date",
+      dataIndex: "Date",
+      sortDirections: ["descend", "ascend"],
+      filterMultiple: false,
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        <div style={{ padding: 10 }} onKeyDown={(e) => e.stopPropagation()}>
+          <Space>
+            <RangePicker
+              value={selectedKeys[0]}
+              onChange={(e) => {
+                setSelectedKeys(e ? [e] : []);
+              }}
+              onPressEnter={() => confirm}
+              style={{ marginBottom: 10, display: "flex" }}
+            />
+          </Space>
+          <Space style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Button
+              type="primary"
+              onClick={() => {
+                confirm();
+              }}
+              icon={<SearchOutlined />}
+              size="small"
+              style={{ width: 90 }}>
+              Search
+            </Button>
+            <Button
+              onClick={() => {
+                clearFilters();
+                confirm();
+              }}
+              size="small"
+              style={{ width: 90 }}>
+              Reset
+            </Button>
+          </Space>
+        </div>
+      ),
+      filterIcon: (filtered: boolean) => <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />,
+      onFilter: (value, record) => {
+        const recordDate = dayjs(record.Date);
+
+        const startDate = value[0];
+        const endDate = value[1];
+        return startDate && endDate
+          ? (recordDate.isAfter(startDate, "day") || recordDate.isSame(startDate, "day")) &&
+              (recordDate.isBefore(endDate, "day") || recordDate.isSame(startDate, "day"))
+          : true;
+      },
+      sorter: (a, b) => {
+        const dateA = new Date(a.Date.toString());
+        const dateB = new Date(b.Date.toString());
+        return dateA.getTime() - dateB.getTime();
+      },
+      render: (value: string) => <div>{dayjs(value).format("YYYY-MM-DD ")}</div>,
+    },
+    {
+      title: "Hour",
+      dataIndex: "Hour",
+      sortDirections: ["descend", "ascend"],
+      sorter: (a, b) => a.Hour - b.Hour,
+      render: (value: string) => <div>{value}</div>,
+    },
+    {
+      title: "OTHour",
+      dataIndex: "OTHour",
+      sortDirections: ["descend", "ascend"],
+      sorter: (a, b) => a.OTHour - b.OTHour,
+      render: (value: string) => <div>{value}</div>,
+    },
+    {
+      title: "Status",
+      dataIndex: "Status",
+      sortDirections: ["descend", "ascend"],
+      sorter: (a, b) => a.Status - b.Status,
+      render: (value: AttendanceStatus) => (
+        <div>
+          <AttendanceStatusTag status={value} />
+        </div>
+      ),
+    },
+    {
+      title: "Type",
+      dataIndex: "Type",
+      sortDirections: ["descend", "ascend"],
+      sorter: (a, b) => a.Type - b.Type,
+      render: (value: AttendanceType) => {
+        switch (value) {
+          case AttendanceType.Online.valueOf():
+            return (
+              <div>
+                <Tag color="success">{AttendanceType[value]} </Tag>
+              </div>
+            );
+          case AttendanceType.Offline.valueOf():
+            return (
+              <div>
+                <Tag color="default">{AttendanceType[value]} </Tag>
+              </div>
+            );
+        }
+      },
+    },
+  ];
+  //#endregion
   useEffect(() => {
     fetchAttendances();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -135,19 +264,37 @@ export const EmpDashBoard: React.FC = () => {
               dayjs(item.Date) <= dayjs().endOf("week")
           ).map((item: { Id: any }) => ({ ...item, key: item.Id }))
         );
-        setAttendancesMonth(
+        setAttendancesMonthly(
           (res as any).Attendances.filter(
             (item: { Status: string; Date: string }) =>
               item.Status === "Approved" &&
               dayjs(item.Date) >= dayjs().startOf("month") &&
               dayjs(item.Date) <= dayjs().endOf("month")
-          ).map((item: { Hour: any; Id: any }) => ({
-            ...item,
-            key: item.Id,
-            Expected: 8,
-            Logged: item.Hour,
-          }))
+          ).map((item: { Id: any }) => ({ ...item, key: item.Id }))
         );
+        const attendanceByWeekly = (res as any).Attendances.filter(
+          (item: { Status: string; Date: string }) =>
+            item.Status === "Approved" &&
+            dayjs(item.Date) >= dayjs().startOf("month") &&
+            dayjs(item.Date) <= dayjs().endOf("month")
+        ).reduce((acc: any, item: { Hour: any; OTHour: any; Id: any; Date: string }) => {
+          const weekStartDate = dayjs(item.Date).startOf("week");
+          const weekEndDate = dayjs(item.Date).endOf("week");
+          const weekKey = `${weekStartDate.format("YYYY-MM-DD")}_${weekEndDate.format("YYYY-MM-DD")}`;
+          if (!acc[weekKey]) {
+            acc[weekKey] = {
+              key: weekKey,
+              Date: weekStartDate,
+              Expected: 0,
+              Logged: 0,
+            };
+          }
+          acc[weekKey].Expected = 40;
+          acc[weekKey].Logged += item.Hour + item.OTHour;
+
+          return acc;
+        }, {});
+        setAttendancesWeekly(Object.values(attendanceByWeekly));
         setContracts((res as any).Contracts.map((item: { Id: any }) => ({ ...item, key: item.Id })));
         {
           const currentWeeksHours = (res as any).Attendances.filter(
@@ -165,7 +312,7 @@ export const EmpDashBoard: React.FC = () => {
         {
           const unApprovedHours = (res as any).Attendances.filter(
             (item: { Status: string; Date: string }) =>
-              item.Status !== "Approved" &&
+              item.Status == "Waiting" &&
               dayjs(item.Date) >= dayjs().startOf("week") &&
               dayjs(item.Date) <= dayjs().endOf("week")
           ).map((item: { Id: any; Hour: any }) => item.Hour);
@@ -208,7 +355,6 @@ export const EmpDashBoard: React.FC = () => {
 
   const ShowDetailContract = (data: ContractModel) => {
     setContractSelect(data);
-    console.log(data);
     setIsModalOpen(true);
   };
 
@@ -250,13 +396,12 @@ export const EmpDashBoard: React.FC = () => {
           <div
             style={{
               marginTop: "2rem",
-            }}
-          >
+            }}>
             <AntdTitle level={4}>Weekly attandance summary (hours)</AntdTitle>
             <Table
               size="small"
-              columns={columns}
-              dataSource={attendancesMonth}
+              columns={columnsWeelyTable}
+              dataSource={attendancesWeekly}
               loading={loading}
               pagination={false}
             />
@@ -265,9 +410,18 @@ export const EmpDashBoard: React.FC = () => {
           <div
             style={{
               marginTop: "2rem",
-            }}
-          >
+            }}>
             <AntdTitle level={4}>Monthly Attendance</AntdTitle>
+            <Table
+              columns={columnsMonthly}
+              dataSource={attendancesMonthly}
+              loading={loading}
+              pagination={{
+                pageSizeOptions,
+                showSizeChanger: true,
+                position: ["topLeft"],
+              }}
+            />
           </div>
         </div>
         <div style={{ width: "40%" }}>
@@ -276,29 +430,21 @@ export const EmpDashBoard: React.FC = () => {
               Logged working hours from {dayjs().startOf("week").format("DD/MM/YYYY")} to{" "}
               {dayjs().endOf("week").format("DD/MM/YYYY")}
             </AntdTitle>
-            <Bar
-              options={optionsChart}
-              data={chartData}
-            />
+            <Bar options={optionsChart} data={chartData} />
           </div>
 
           <div
             style={{
               marginTop: "2rem",
-            }}
-          >
+            }}>
             <AntdTitle level={4}>Current Active Contract</AntdTitle>
             <Row gutter={24}>
               {contracts.map((item) => (
-                <Col
-                  key={item.Id}
-                  span={12}
-                >
+                <Col key={item.Id} span={12}>
                   <Card
                     title={dayjs(item.StartDate).format("DD-MM-YYYY") + "~" + dayjs(item.EndDate).format("DD-MM-YYYY")}
                     onClick={() => ShowDetailContract(item)}
-                    style={{ cursor: "pointer" }}
-                  >
+                    style={{ cursor: "pointer" }}>
                     <Tag>{item.EmployeeType}</Tag>
                     <br />
                     <br />
@@ -317,24 +463,14 @@ export const EmpDashBoard: React.FC = () => {
           </div>
         </div>
       </div>
-      <Modal
-        title="Detail Contract"
-        footer={false}
-        open={isModalOpen}
-        width="80%"
-        onCancel={handleCancel}
-      >
-        <Descriptions
-          bordered
-          column={6}
-          layout="vertical"
-        >
+      <Modal title="Detail Contract" footer={false} open={isModalOpen} width="80%" onCancel={handleCancel}>
+        <Descriptions bordered column={6} layout="vertical">
           <Descriptions.Item label="Employee Type">{contractSelect?.EmployeeType}</Descriptions.Item>
           <Descriptions.Item label="Start Date">
             {dayjs(contractSelect?.StartDate).format("DD-MM-YYYY")}
           </Descriptions.Item>
           <Descriptions.Item label="End Date"> {dayjs(contractSelect?.EndDate).format("DD-MM-YYYY")}</Descriptions.Item>
-          <Descriptions.Item label="Base Salary">{contractSelect?.BaseSalary}$</Descriptions.Item>
+          <Descriptions.Item label="Base Salary">{contractSelect?.BaseSalary} VND</Descriptions.Item>
           <Descriptions.Item label="Date Off Per Year">{contractSelect?.DateOffPerYear}</Descriptions.Item>
           <Descriptions.Item label="Level">{contractSelect?.Level.LevelName}</Descriptions.Item>
           <Descriptions.Item label="Insurance Rate">{contractSelect?.InsuranceRate}%</Descriptions.Item>
